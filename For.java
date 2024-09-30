e040b9ec334420d406b3a02943509994cdeda0b0
marianicacio <mariaeduarda.nicacio2007@gmail.com>
2024-09-30 14:51:49 -0300

    feat: for example

---
+++ For.java
@@ -0,0 +1,16 @@
+
+public class For {
+    //Printar 11 primeiros números
+
+    public static void main(String[] args) {
+        //Declaração do contador
+        int i;
+        //Iniciando o looping com valores de 0 atá 10
+        for (i = 0; i < 100; i++) {
+            //printar o contador
+            System.out.println(i);
+        }
+
+    }
+
+}
