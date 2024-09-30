080f947a54f397f34a46a01bb3100113cc93f1d0
marianicacio <mariaeduarda.nicacio2007@gmail.com>
2024-09-30 16:49:38 -0300

    feat: new mode tabuada

--- Tabuada.java
+++ Tabuada.java
@@ -1,20 +1,22 @@
-import java.util.Scanner;
+package aula8;
 
 public class Tabuada {
 
 	public static void main(String[] args) {
 		int i, tabuada, res;
 		res = 0;
-		Scanner sc = new Scanner(System.in);
-		System.out.print("Digite qual tabuada vc deseja: ");
-		tabuada = sc.nextInt();
+//		Scanner sc = new Scanner(System.in);
+//		System.out.print("Digite qual tabuada vc deseja: ");
+//		tabuada = sc.nextInt();
 		
 		//iniciando a estrutura de repetição:
-		for(i=1; i<=10; i++) {
+		
+		for(tabuada = 1; tabuada <= 10; tabuada++) {
+			for(i=1; i<=10; i++) {
 			res = tabuada * i;
-			System.out.println(tabuada + " X " + i + " = " + res);
-		}
-		System.out.printf("%d|n", res);
+			System.out.println(tabuada + "x" + i + " = " + res);
+		}System.out.printf("\n");
 	}
+}
 
 }
